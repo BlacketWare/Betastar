9e0bfd72d1378d4a66587a39734799e3a936aed2
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2023-01-25 08:18:15 -0500

    sell all elements

---
+++ elements/sellAllElements.js
@@ -0,0 +1,16 @@
+if (confirm('Are you use you want to do this? This is a very dangerous action.\nScript by VillainsRule.')) {
+  alert('Check the console for your sell history :skul:');
+
+  $.get('/api/user/elements', function(data) {
+    window.userElements = JSON.parse(data);
+    Object.keys(elementList).forEach(element => sell(element));
+  });
+
+  async function sell(element) {
+    var amt = window.userElements[element];
+    if (-1 >= amt) return;
+    $.post(`/api/sell/`, `element=${element}&quantity=${amt}`, function() {
+      isNaN(amt) ? "" : console.log(`Sold ${amt} ${element}(s)`);
+    });
+  };
+} else alert('Script cancelled.');
